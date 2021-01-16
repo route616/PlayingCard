94aeeb63f3077a4bc69b11a71b3d633a60939d1e
Igor <route616@gmail.com>
2021-01-16 22:50:36 +0300

    Initial Commit

---
+++ PlayingCArd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayingCArd
+//
+//  Created by Игорь on 16.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
